26ec609942b89179ef21ae416dec0553765f8ae0
Joseff361 <joseff361@outlook.com>
2021-03-19 02:55:23 -0500

    ready to prod

--- src/main/java/com/dorian/imagegallery/controller/CloudinaryController.java
+++ src/main/java/com/dorian/imagegallery/controller/CloudinaryController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,7 @@ import com.dorian.imagegallery.service.CloudinaryService;
 import com.dorian.imagegallery.service.ImageService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.DELETE})
 @RequestMapping("cloudinary")
 public class CloudinaryController {
 
